bea5fcda35fe8b80d18f40e758bc81f6969cfbb7
Evgeny Galitskiy <gha665@gmail.com>
2020-06-14 16:26:25 -0400

    added first API and query functionality, added grsphiql testing tool

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
 app.use(bodyParser.json());
 
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]! 
+        }
 
-app.listen(3000); 
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery,
+            mutation: RootMutation
+        }
+    `),
+
+    // resolver
+    rootValue: {
+      events: () => {
+        return ["Romantic Cooking", "Sailing", "All-Night Coding"];
+      },
+
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+
+    // debugging development tool that apperas in browser, where we can play around and test the app
+    // "in-browser tool for writing, validating, and testing GraphQL queries"
+    graphiql: true,
+  })
+);
+
+app.listen(3000);
