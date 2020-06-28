8d004cb22c0ef79728a4165e1b9b5848399efd20
Evgeny Galitskiy <gha665@gmail.com>
2020-06-27 22:59:06 -0400

    formatted promises into async/await

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,56 +3,54 @@ const bcrypt = require("bcryptjs");
 const Event = require("../../models/event");
 const User = require("../../models/user");
 
-const events = (eventIds) => {
-  return Event.find({ id: { $in: eventIds } })
-    .then((events) => {
-      return events.map((event) => {
-        return {
-          ...event._doc,
-          _id: event.id,
-          date: new Date(event._doc.date).toISOString(),
-          creator: user.bind(this, event.creator),
-        };
-      });
-    })
-    .catch((err) => {
-      throw err;
+const events = async (eventIds) => {
+  try {
+    const events = await Event.find({ _id: { $in: eventIds } });
+    events.map((event) => {
+      return {
+        ...event._doc,
+        _id: event.id,
+        date: new Date(event._doc.date).toISOString(),
+        creator: user.bind(this, event.creator),
+      };
     });
+    return events;
+  } catch (err) {
+    throw err;
+  }
 };
 
-const user = (userId) => {
-  return User.findById(userId)
-    .then((user) => {
-      return {
-        ...user._doc,
-        _id: user.id,
-        createdEvents: events.bind(this, user._doc.createdEvents),
-      };
-    })
-    .catch((err) => {
-      throw err;
-    });
+const user = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    return {
+      ...user._doc,
+      _id: user.id,
+      createdEvents: events.bind(this, user._doc.createdEvents),
+    };
+  } catch (err) {
+    throw err;
+  }
 };
 
 module.exports = {
-  events: () => {
-    return Event.find()
-      .then((events) => {
-        return events.map((event) => {
-          return {
-            ...event._doc,
-            _id: event.id, // <--- <<<_id: EVENT.ID>>> is a shortcut provided by Mongoose to read the id. But result._doc._id.toString() is also effective.
-            date: new Date(event._doc.date).toISOString(),
-            creator: user.bind(this, event._doc.creator),
-          };
-        });
-      })
-      .catch((err) => {
-        throw err;
+  events: async () => {
+    try {
+      const events = await Event.find();
+      return events.map((event) => {
+        return {
+          ...event._doc,
+          _id: event.id, // <--- <<<_id: EVENT.ID>>> is a shortcut provided by Mongoose to read the id. But result._doc._id.toString() is also effective.
+          date: new Date(event._doc.date).toISOString(),
+          creator: user.bind(this, event._doc.creator),
+        };
       });
+    } catch (err) {
+      throw err;
+    }
   },
 
-  createEvent: (args) => {
+  createEvent: async (args) => {
     // const event = {
     //   _id: Math.random().toString(),
     //   title: args.eventInput.title,
@@ -66,61 +64,55 @@ module.exports = {
       description: args.eventInput.description,
       price: +args.eventInput.price,
       date: new Date(args.eventInput.date),
-      creator: "5ef16569a0b9ac0ae7fe8f52",
+      creator: "5ef804f31f89223a56e43688",
     });
 
     let createdEvent;
 
-    // events.push(event); <--- before constructor
-    return event
-      .save() // <--- with constructor
-      .then((result) => {
-        createdEvent = {
-          ...result._doc,
-          id: result._doc._id.toString(), // <--- OR _id: event.id
-          date: new Date(event._doc.date).toISOString(),
-          creator: user.bind(this, result._doc.creator),
-        };
-        return User.findById("5ef16569a0b9ac0ae7fe8f52");
-      })
-      .then((user) => {
-        if (!user) {
-          throw new Error("User not found.");
-        }
-        user.createdEvents.push(event);
-        return user.save();
-      })
-      .then((result) => {
-        return createdEvent;
-      })
-      .catch((err) => {
-        console.log(err);
-        throw err;
-      });
-    return event; // <--- before constructor
+    try {
+      // events.push(event); <--- before constructor
+      const result = await event.save(); // <--- with constructor
+      createdEvent = {
+        ...result._doc,
+        _id: result._doc._id.toString(), // <--- OR _id: event.id
+        date: new Date(event._doc.date).toISOString(),
+        creator: user.bind(this, result._doc.creator),
+      };
+      const creator = await User.findById("5ef804f31f89223a56e43688");
+
+      if (!creator) {
+        throw new Error("User not found.");
+      }
+      creator.createdEvents.push(event);
+      await creator.save();
+
+      return createdEvent;
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+    // return event; // <--- before constructor
   },
 
-  createUser: (args) => {
-    // to eliminate all non-unique users
-    return User.findOne({ email: args.userInput.email })
-      .then((user) => {
-        if (user) {
-          throw new Error("User already exists.");
-        }
-        return bcrypt.hash(args.userInput.password, 12);
-      })
-      .then((hashedPassword) => {
-        const user = new User({
-          email: args.userInput.email,
-          password: hashedPassword,
-        });
-        return user.save();
-      })
-      .then((result) => {
-        return { ...result._doc, password: null, _id: result.id };
-      })
-      .catch((err) => {
-        throw err;
+  createUser: async (args) => {
+    try {
+      // to eliminate all non-unique users
+      const existingUser = await User.findOne({ email: args.userInput.email });
+      if (existingUser) {
+        throw new Error("User exists already.");
+      }
+      const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
+
+      const user = new User({
+        email: args.userInput.email,
+        password: hashedPassword,
       });
+
+      const result = await user.save();
+
+      return { ...result._doc, password: null, _id: result.id };
+    } catch (err) {
+      throw err;
+    }
   },
 };
