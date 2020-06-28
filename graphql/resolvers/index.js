5f27b3990399064a9767c5054073d615f115b945
Evgeny Galitskiy <gha665@gmail.com>
2020-06-28 17:13:37 -0400

    added drilling into user and creator functionality

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -21,6 +21,19 @@ const events = async (eventIds) => {
   }
 };
 
+const singleEvent = async (eventId) => {
+  try {
+    const event = await Event.findById(eventId);
+    return {
+      ...event._doc,
+      id: eventId,
+      creator: user.bind(this, event.creator),
+    };
+  } catch (err) {
+    throw err;
+  }
+};
+
 const user = async (userId) => {
   try {
     const user = await User.findById(userId);
@@ -58,6 +71,8 @@ module.exports = {
         return {
           ...booking._doc,
           _id: booking.id,
+          user: user.bind(this, booking._doc.user),
+          event: singleEvent.bind(this, booking._doc.event),
           createdAt: new Date(booking._doc.createdAt).toISOString(),
           updatedAt: new Date(booking._doc.updatedAt).toISOString(),
         };
@@ -143,6 +158,8 @@ module.exports = {
     return {
       ...result.doc,
       _id: result.id,
+      user: user.bind(this, booking._doc.user),
+      event: singleEvent.bind(this, booking._doc.event),
       createdAt: new Date(result._doc.createdAt).toISOString(),
       updatedAt: new Date(result._doc.updatedAt).toISOString(),
     };
