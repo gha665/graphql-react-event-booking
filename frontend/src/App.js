93ce12f0f6c69dc56b78ee5e3c3042ffab3642d0
Evgeny Galitskiy <gha665@gmail.com>
2020-07-14 23:56:59 -0400

    created events and bookings pages, updated routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+
+import AuthPage from "./pages/Auth";
+import BookingsPage from "./pages/Bookings";
+import EventsPage from "./pages/Events";
+
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" component={null} />
-      <Route path="/auth" component={null} />
-      <Route path="/events" component={null} />
-      <Route path="/bookings" component={null} />
+      <Switch>
+        <Redirect from="/" to="/auth" exact />
+        <Route path="/auth" component={AuthPage} />
+        <Route path="/events" component={EventsPage} />
+        <Route path="/bookings" component={BookingsPage} />
+      </Switch>
     </BrowserRouter>
   );
 }
