7d9d3e01ba995582bcdcaea2b6c76402de252759
Evgeny Galitskiy <gha665@gmail.com>
2020-07-26 15:20:21 -0400

    added context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,25 +5,49 @@ import AuthPage from "./pages/Auth";
 import BookingsPage from "./pages/Bookings";
 import EventsPage from "./pages/Events";
 import MainNavigation from "./components/Navigation/MainNavigation";
+import AuthContext from "./context/auth-context";
 
 import "./App.css";
+import user from "../../models/user";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <React.Fragment>
-        <MainNavigation />
-        <main className="main-content">
-          <Switch>
-            <Redirect from="/" to="/auth" exact />
-            <Route path="/auth" component={AuthPage} />
-            <Route path="/events" component={EventsPage} />
-            <Route path="/bookings" component={BookingsPage} />
-          </Switch>
-        </main>
-      </React.Fragment>
-    </BrowserRouter>
-  );
+class App extends Component {
+  state = {
+    token: null,
+    userId: null,
+  };
+
+  login = (token, userId, tokenExpiraion) => {
+    this.setState({ token: token, userId: userId });
+  };
+
+  logout = () => {};
+
+  render() {
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <AuthContext.Provider
+            value={{
+              token: null,
+              userId: null,
+              login: this.login,
+              logout: this.logout,
+            }}
+          >
+            <MainNavigation />
+            <main className="main-content">
+              <Switch>
+                <Redirect from="/" to="/auth" exact />
+                <Route path="/auth" component={AuthPage} />
+                <Route path="/events" component={EventsPage} />
+                <Route path="/bookings" component={BookingsPage} />
+              </Switch>
+            </main>
+          </AuthContext.Provider>
+        </React.Fragment>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
