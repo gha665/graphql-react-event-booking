51406ee6f6e751ed6ac7b2abf3586de35e9aedc5
Evgeny Galitskiy <gha665@gmail.com>
2020-07-31 23:48:21 -0400

    added backdrop componentt, fixed modal style

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Modal from "../components/Modal/Modal";
+import Backdrop from "../components/Backdrop/Backdrop";
 
 import "./Events.css";
 
@@ -7,6 +8,7 @@ class EventsPage extends Component {
   render() {
     return (
       <React.Fragment>
+        <Backdrop />
         <Modal title="Add Event" canCancel canConfirm>
           <p>Modal Content</p>
         </Modal>
