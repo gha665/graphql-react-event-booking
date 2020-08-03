d84b621dc7f12bff13b993a7f3f9bbaa759ae9f6
Evgeny Galitskiy <gha665@gmail.com>
2020-08-02 22:40:22 -0400

    created form for events

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -33,7 +33,24 @@ class EventsPage extends Component {
             onCancel={this.modalCancelHandler}
             onConfirm={this.modalConfirmHandler}
           >
-            <p>Modal Content</p>
+            <form>
+              <div className="form-control">
+                <label htmlFor="title">Title</label>
+                <input type="text" id="title"></input>
+              </div>
+              <div className="form-control">
+                <label htmlFor="price">Price</label>
+                <input type="number" id="price"></input>
+              </div>
+              <div className="form-control">
+                <label htmlFor="date">Date</label>
+                <input type="date" id="date"></input>
+              </div>
+              <div className="form-control">
+                <label htmlFor="description">Description</label>
+                <textarea id="description" rows="4"></textarea>
+              </div>
+            </form>
           </Modal>
         )}
         <div className="events-control">
