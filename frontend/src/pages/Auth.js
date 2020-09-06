643b282ea2146ce4d8c8dcd0c8c14123eccbee05
Evgeny Galitskiy <gha665@gmail.com>
2020-09-05 23:36:42 -0400

    fixed login issue in app.js

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 
 import "./Auth.css";
-// import { json } from "body-parser";
 import AuthContext from "../context/auth-context";
 
 class AuthPage extends Component {
@@ -11,14 +10,12 @@ class AuthPage extends Component {
 
   static contextType = AuthContext;
 
-  // REFERENCES TO SEND REQUESTS
   constructor(props) {
     super(props);
     this.emailEl = React.createRef();
     this.passwordEl = React.createRef();
   }
 
-  // STATE TOGGLER FOR LOGGED IN
   switchModeHandler = () => {
     this.setState((prevState) => {
       return { isLogin: !prevState.isLogin };
@@ -26,7 +23,7 @@ class AuthPage extends Component {
   };
 
   submitHandler = (event) => {
-    event.preventDefault(); // <--- to make sure no request gets sent
+    event.preventDefault();
     const email = this.emailEl.current.value;
     const password = this.passwordEl.current.value;
 
@@ -34,8 +31,6 @@ class AuthPage extends Component {
       return;
     }
 
-    // SEND TADA TO SERVER
-    // Log in
     let requestBody = {
       query: `
         query {
@@ -48,7 +43,6 @@ class AuthPage extends Component {
       `,
     };
 
-    // Sign Up
     if (!this.state.isLogin) {
       requestBody = {
         query: `
@@ -96,12 +90,10 @@ class AuthPage extends Component {
           <label htmlFor="email">E-Mail</label>
           <input type="email" id="email" ref={this.emailEl} />
         </div>
-
         <div className="form-control">
           <label htmlFor="password">Password</label>
           <input type="password" id="password" ref={this.passwordEl} />
         </div>
-
         <div className="form-actions">
           <button type="submit">Submit</button>
           <button type="button" onClick={this.switchModeHandler}>
@@ -114,3 +106,120 @@ class AuthPage extends Component {
 }
 
 export default AuthPage;
+
+// import React, { Component } from "react";
+
+// import "./Auth.css";
+// // import { json } from "body-parser";
+// import AuthContext from "../context/auth-context";
+
+// class AuthPage extends Component {
+//   state = {
+//     isLogin: true,
+//   };
+
+//   static contextType = AuthContext;
+
+//   // REFERENCES TO SEND REQUESTS
+//   constructor(props) {
+//     super(props);
+//     this.emailEl = React.createRef();
+//     this.passwordEl = React.createRef();
+//   }
+
+//   // STATE TOGGLER FOR LOGGED IN
+//   switchModeHandler = () => {
+//     this.setState((prevState) => {
+//       return { isLogin: !prevState.isLogin };
+//     });
+//   };
+
+//   submitHandler = (event) => {
+//     event.preventDefault(); // <--- to make sure no request gets sent
+//     const email = this.emailEl.current.value;
+//     const password = this.passwordEl.current.value;
+
+//     if (email.trim().length === 0 || password.trim().length === 0) {
+//       return;
+//     }
+
+//     // SEND TADA TO SERVER
+//     // Log in
+//     let requestBody = {
+//       query: `
+//         query {
+//           login(email: "${email}", password: "${password}") {
+//             userId
+//             token
+//             tokenExpiration
+//           }
+//         }
+//       `,
+//     };
+
+//     // Sign Up
+//     if (!this.state.isLogin) {
+//       requestBody = {
+//         query: `
+//           mutation {
+//             createUser(userInput: {email: "${email}", password: "${password}"}) {
+//               _id
+//               email
+//             }
+//           }
+//         `,
+//       };
+//     }
+
+//     fetch("http://localhost:8000/graphql", {
+//       method: "POST",
+//       body: JSON.stringify(requestBody),
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     })
+//       .then((res) => {
+//         if (res.status !== 200 && res.status !== 201) {
+//           throw new Error("Failed!");
+//         }
+//         return res.json();
+//       })
+//       .then((resData) => {
+//         if (resData.data.login.token) {
+//           this.context.login(
+//             resData.data.login.token,
+//             resData.data.login.userId,
+//             resData.data.login.tokenExpiration
+//           );
+//         }
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   };
+
+//   render() {
+//     return (
+//       <form className="auth-form" onSubmit={this.submitHandler}>
+//         <div className="form-control">
+//           <label htmlFor="email">E-Mail</label>
+//           <input type="email" id="email" ref={this.emailEl} />
+//         </div>
+
+//         <div className="form-control">
+//           <label htmlFor="password">Password</label>
+//           <input type="password" id="password" ref={this.passwordEl} />
+//         </div>
+
+//         <div className="form-actions">
+//           <button type="submit">Submit</button>
+//           <button type="button" onClick={this.switchModeHandler}>
+//             Switch to {this.state.isLogin ? "Signup" : "Login"}
+//           </button>
+//         </div>
+//       </form>
+//     );
+//   }
+// }
+
+// export default AuthPage;
