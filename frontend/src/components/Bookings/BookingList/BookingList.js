9d774684a86635cd09e72c03f76c6029b573b787
Evgeny Galitskiy <gha665@gmail.com>
2020-09-14 00:01:26 -0400

    added cancel bookings

--- frontend/src/components/Bookings/BookingList/BookingList.js
+++ frontend/src/components/Bookings/BookingList/BookingList.js
@@ -12,7 +12,12 @@ const bookingList = (props) => (
             {new Date(booking.createdAt).toLocaleDateString()}
           </div>
           <div className="bookings__item-actions">
-            <button className="btn">Cancel</button>
+            <button
+              className="btn"
+              onClick={props.onDelete.bind(this, booking._id)}
+            >
+              Cancel
+            </button>
           </div>
         </li>
       );
